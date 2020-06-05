79956a0f0bad1b80ef7164bea5d1f67956942853
Michal <michalogloza97@gmail.com>
2020-06-05 19:58:06 +0200

    3.2 Added addingLikePostByUserWithNonConfirmedStatusShouldThrowDomainError test.

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -2,8 +2,14 @@ package edu.iis.mto.blog.domain;
 
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -20,12 +26,20 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
 
     @MockBean
     private UserRepository userRepository;
+
+    @MockBean
+    private BlogPostRepository postRepository;
+
+    @MockBean
+    private LikePostRepository likeRepository;
 
     @Autowired
     private BlogDataMapper dataMapper;
@@ -42,6 +56,26 @@ public class BlogManagerTest {
         verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+    @Ignore
+    @Test(expected = DomainError.class)
+    public void addingLikePostByUserWithNonConfirmedStatusShouldThrowDomainError() {
+        User user = new User();
+        user.setId(1L);
+        user.setAccountStatus(AccountStatus.NEW);
+
+        User author = new User();
+        author.setId(2L);
+
+        BlogPost post = new BlogPost();
+        post.setId(1L);
+        post.setUser(author);
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
+        when(likeRepository.findByUserAndPost(user, post)).thenReturn(Optional.empty());
+
+        blogService.addLikeToPost(user.getId(), post.getId());
     }
 
 }
