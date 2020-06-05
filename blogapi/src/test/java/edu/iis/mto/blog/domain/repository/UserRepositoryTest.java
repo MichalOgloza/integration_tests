e41e479612d083db7cd5391ca6a32fdbea3e850e
Michal <michalogloza97@gmail.com>
2020-06-05 15:25:25 +0200

    3.1.2 added UserRepository tests for find method.

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package edu.iis.mto.blog.domain.repository;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import java.util.List;
@@ -30,6 +31,7 @@ public class UserRepositoryTest {
     private UserRepository repository;
 
     private User user;
+    private User user2;
 
     @Before
     public void setUp() {
@@ -37,6 +39,10 @@ public class UserRepositoryTest {
         user.setFirstName("Jan");
         user.setEmail("john@domain.com");
         user.setAccountStatus(AccountStatus.NEW);
+        user2 = new User();
+        user2.setFirstName("Piotr");
+        user2.setEmail("nowak@domain.com");
+        user2.setAccountStatus(AccountStatus.NEW);
     }
 
     @Test
@@ -64,6 +70,49 @@ public class UserRepositoryTest {
         User persistedUser = repository.save(user);
 
         assertThat(persistedUser.getId(), notNullValue());
+    }
+
+    @Test
+    public void findSpecificUserTest() {
+        User persistedUser = entityManager.persist(user);
+        entityManager.persist(user2);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("Jan", "", "john@domain.com");
+        assertThat(users, hasSize(1));
+        assertEquals(users.get(0), persistedUser);
+    }
+
+    @Test
+    public void findUserContainingSpecifiedStringsInNameAndEmailTest() {
+        User persistedUser = entityManager.persist(user);
+        entityManager.persist(user2);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("an", "", "hn");
+        assertThat(users, hasSize(1));
+        assertEquals(users.get(0), persistedUser);
+    }
+
+    @Test
+    public void findUserIgnoreCaseTest() {
+        User persistedUser = entityManager.persist(user);
+        entityManager.persist(user2);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("jAN", "", "JOHN@dOmAiN.cOm");
+        assertThat(users, hasSize(1));
+        assertEquals(users.get(0), persistedUser);
+    }
+
+    @Test
+    public void userNotFoundTest() {
+        User persistedUser = entityManager.persist(user);
+        entityManager.persist(user2);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("Janusz", "", "kowalski@domain.com");
+        assertThat(users, hasSize(0));
+    }
+
+    @Test
+    public void findFirstUserByNameAndSecondByEmail() {
+        User persistedUser = entityManager.persist(user);
+        entityManager.persist(user2);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("Jan", "", "nowak@domain.com");
+        assertThat(users, hasSize(2));
     }
 
 }
