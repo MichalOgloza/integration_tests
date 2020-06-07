315dfcb43f3d8847adbc0240caf901dbe0c336bb
Michal <michalogloza97@gmail.com>
2020-06-07 18:17:11 +0200

    3.4.3 Added multiple likes from same user not changes like count test.

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -1,6 +1,8 @@
 package edu.iis.mto.blog.rest.test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
@@ -15,6 +17,7 @@ public class LikePostTest extends FunctionalTests {
     private static final long NEW_USER_ID = 2L;
     private static final long FIRST_USER_POST_ID = 1L;
     private static final long SECOND_USER_POST_ID = 2L;
+    private static final long LIKED_POST_ID = 3L;
 
 
     @Test
@@ -51,6 +54,29 @@ public class LikePostTest extends FunctionalTests {
                 .statusCode(HttpStatus.SC_BAD_REQUEST)
                 .when()
                 .post(USER_API + "/" + CONFIRMED_USER_ID + "/like/" + FIRST_USER_POST_ID);
+    }
+
+    @Test
+    public void likePostAgainNotChangesLikeCount() {
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post(USER_API + "/" + CONFIRMED_USER_ID + "/like/" + LIKED_POST_ID);
+
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .get("blog/post/" + LIKED_POST_ID)
+                .then()
+                .body("likesCount",is(equalTo(1)));
     }
 }
 
